b150956df557f05cf6078e4c7dce9370a388651f
Ankit Kumar <ankikuma@microsoft.com>
2020-11-24 15:40:59 -0800

    grpc tests: specify port and log

--- src/WebJobs.Script.Grpc/Server/GrpcServer.cs
+++ src/WebJobs.Script.Grpc/Server/GrpcServer.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Script.Eventing;
 using Microsoft.Azure.WebJobs.Script.Workers;
 using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Script.Grpc
 {
@@ -15,31 +16,43 @@ namespace Microsoft.Azure.WebJobs.Script.Grpc
         private readonly IHostBuilder _grpcHostBuilder;
         private readonly int _port;
         private bool _disposed = false;
+        private ILogger _logger;
         private IHost _grpcHost;
 
-        public GrpcServer(IScriptEventManager scriptEventManager)
+        public GrpcServer(IScriptEventManager scriptEventManager, ILogger<GrpcServer> logger)
         {
             _port = WorkerUtilities.GetUnusedTcpPort();
             _grpcHostBuilder = GrpcHostBuilder.CreateHostBuilder(scriptEventManager, _port);
+            _grpcHost = _grpcHostBuilder.Build();
+            _grpcHost.Start();
+            _logger = logger;
         }
 
         public Uri Uri => new Uri($"http://{WorkerConstants.HostName}:{_port}");
 
         public Task StartAsync()
         {
-            _grpcHost = _grpcHostBuilder.Build();
-            _grpcHost.Start();
+            _logger.LogInformation($"Grpc service started at port {_port}");
             return Task.CompletedTask;
         }
 
-        public Task ShutdownAsync() => _grpcHost.StopAsync();
+        public Task ShutdownAsync()
+        {
+            _logger.LogInformation($"Grpc service is being ShutDown");
+            return _grpcHost.StopAsync();
+        }
 
-        public Task KillAsync() => _grpcHost.StopAsync();
+        public Task KillAsync()
+        {
+            _logger.LogInformation($"Grpc service is being killed");
+            return _grpcHost.StopAsync();
+        }
 
         protected async ValueTask DisposeAsync(bool disposing)
         {
             if (!_disposed)
             {
+                _logger.LogInformation($"Grpc service is being disposed");
                 if (disposing)
                 {
                     await _grpcHost.StopAsync();
--- src/WebJobs.Script/Workers/WorkerUtilities.cs
+++ src/WebJobs.Script/Workers/WorkerUtilities.cs
@@ -10,12 +10,13 @@ namespace Microsoft.Azure.WebJobs.Script.Workers
     {
         public static int GetUnusedTcpPort()
         {
-            using (Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-            {
-                tcpSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
-                int port = ((IPEndPoint)tcpSocket.LocalEndPoint).Port;
-                return port;
-            }
+            //using (Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            //{
+            //    tcpSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+            //    int port = ((IPEndPoint)tcpSocket.LocalEndPoint).Port;
+            //    return port;
+            //}
+            return 7089;
         }
     }
 }
