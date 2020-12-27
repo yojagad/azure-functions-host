b789e31063312b964fad7b8ab7a0023b8f848be1
Divya Gandhi <digandhi@microsoft.com>
2020-12-26 20:52:00 -0800

    add logging for k8se

---
+++ src/WebJobs.Script.WebHost/Diagnostics/KubernetesEventGenerator.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
+{
+    internal class KubernetesEventGenerator : LinuxEventGenerator
+    {
+        private const int MaxDetailsLength = 10000;
+        private readonly IEnvironment _environment;
+        private string _podName;
+
+        public KubernetesEventGenerator(IEnvironment environment)
+        {
+            _environment = environment;
+            _podName = _environment.GetEnvironmentVariable(EnvironmentSettingNames.PodNamespace);
+        }
+
+        public override void LogFunctionTraceEvent(LogLevel level, string subscriptionId, string appName, string functionName, string eventName, string source, string details, string summary, string exceptionType, string exceptionMessage, string functionInvocationId, string hostInstanceId, string activityId, string runtimeSiteName, string slotName, DateTime eventTimestamp)
+        {
+            string formattedEventTimeStamp = eventTimestamp.ToString(EventTimestampFormat);
+            string hostVersion = ScriptHost.Version;
+            FunctionsSystemLogsEventSource.Instance.SetActivityId(activityId);
+            details = details.Length > MaxDetailsLength ? details.Substring(0, MaxDetailsLength) : details;
+
+            Console.WriteLine($"{ScriptConstants.LinuxLogEventStreamName} {(int)ToEventLevel(level)},{subscriptionId},{appName},{functionName},{eventName},{source},{NormalizeString(details)},{NormalizeString(summary)},{hostVersion},{formattedEventTimeStamp},{exceptionType},{NormalizeString(exceptionMessage)},{functionInvocationId},{hostInstanceId},{activityId},{runtimeSiteName},{slotName}");
+        }
+
+        public override void LogFunctionMetricEvent(string subscriptionId, string appName, string functionName, string eventName, long average, long minimum, long maximum, long count, DateTime eventTimestamp, string data, string runtimeSiteName, string slotName)
+        {
+            string hostVersion = ScriptHost.Version;
+
+            Console.WriteLine($"{ScriptConstants.LinuxMetricEventStreamName} {subscriptionId},{appName},{functionName},{eventName},{average},{minimum},{maximum},{count},{hostVersion},{eventTimestamp.ToString(EventTimestampFormat)},{NormalizeString(data)},{runtimeSiteName},{slotName}");
+        }
+
+        public override void LogFunctionDetailsEvent(string siteName, string functionName, string inputBindings, string outputBindings, string scriptType, bool isDisabled)
+        {
+            Console.WriteLine($"{ScriptConstants.LinuxFunctionDetailsEventStreamName} {siteName},{functionName},{NormalizeString(inputBindings)},{NormalizeString(outputBindings)},{scriptType},{(isDisabled ? 1 : 0)}");
+        }
+
+        public override void LogFunctionExecutionAggregateEvent(string siteName, string functionName, long executionTimeInMs, long functionStartedCount, long functionCompletedCount, long functionFailedCount)
+        {
+        }
+
+        public override void LogFunctionExecutionEvent(string executionId, string siteName, int concurrency, string functionName, string invocationId, string executionStage, long executionTimeSpan, bool success)
+        {
+        }
+
+        public override void LogAzureMonitorDiagnosticLogEvent(LogLevel level, string resourceId, string operationName, string category, string regionName, string properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
