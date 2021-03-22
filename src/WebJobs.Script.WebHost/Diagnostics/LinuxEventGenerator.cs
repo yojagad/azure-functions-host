7db311211c0cdc703823f6670d24d304c32f0981
Divya Gandhi <digandhi@microsoft.com>
2021-03-22 14:44:12 -0700

    update2

--- src/WebJobs.Script.WebHost/Diagnostics/LinuxEventGenerator.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/LinuxEventGenerator.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
         public static readonly string EventTimestampFormat = "O";
 
         // These names should match the source file names for fluentd
-        public static readonly string FunctionsLogsCategory = "functionslogsv2";
+        public static readonly string FunctionsLogsCategory = "functionslogs";
         public static readonly string FunctionsMetricsCategory = "functionsmetrics";
         public static readonly string FunctionsDetailsCategory = "functionsdetails";
         public static readonly string FunctionsExecutionEventsCategory = "functionexecutionevents";
